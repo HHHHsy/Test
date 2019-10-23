1f44b5e9c1089433e199cb304e54c18f9e87441a
HHHHsy <ysh520+->
2019-10-23 16:09:52 +0800

    daixu

---
+++ src/main/java/com/gzy/im/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.gzy.im.controller;
+
+
+import com.gzy.im.entity.Token;
+import com.gzy.im.entity.User;
+import com.gzy.im.repository.TokenRepository;
+import com.gzy.im.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+
+    @Resource
+    TokenRepository tokenRepository;
+
+    @Resource
+    UserRepository userRepository;
+
+    @GetMapping("/userifo")
+    Object userifo(Long userid,String token){
+        Token tokenByToken =  tokenRepository.findTokenByToken(token);
+
+        if (tokenByToken==null) {
+            return ResponseEntity.status(401).build();
+        }
+        if (!tokenByToken.getId().equals(userid)) {
+            return ResponseEntity.status(401).build();
+        }
+        Optional<User> userOptional = userRepository.findById(userid);
+        if (userOptional.isPresent()) {
+            return null;
+        }
+
+
+
+        return null;
+    }
+}
